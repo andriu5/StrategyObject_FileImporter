9899f18402ea54a084339901ddabb8ac8852beb7
aalvear <aalvear@cognitiva.la>
2021-11-19 22:50:04 -0300

    REF making all clases available in the module level

--- ImportEngine/__init__.py
+++ ImportEngine/__init__.py
@@ -1,4 +1,6 @@
-from .DocxImporter import DocxImporter
+from .XlsxImporter import XlsxImporter
 from .CSVImporter import CSVImporter
+from .TxtImporter import TxtImporter
+from .DocxImporter import DocxImporter
 from .PDFImporter import PDFImporter
 from .Importer import Importer
